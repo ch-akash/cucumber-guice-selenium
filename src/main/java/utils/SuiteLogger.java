04de129e269c7f1a28758873f095c9b9e30f74ce
Akash <akashchaudhary1222@gmail.com>
2021-02-28 11:21:16 +0530

    changes to readme

---
+++ src/main/java/utils/SuiteLogger.java
@@ -0,0 +1,14 @@
+package main.java.utils;
+
+import io.cucumber.plugin.ConcurrentEventListener;
+import io.cucumber.plugin.event.EventPublisher;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+import org.testng.internal.ITestResultNotifier;
+import org.testng.log4testng.Logger;
+
+import java.io.Serializable;
+
+public class SuiteLogger {
+
+}
